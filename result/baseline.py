75342c15d14c9473860091c347e97563aaba2c53
laomd-2 <laomd@mail2.sysu.edu.cn>
2019-06-09 22:25:25 +0800

    结果

--- result/baseline.py
+++ result/baseline.py
@@ -3,18 +3,23 @@ import sklearn.cluster
 import scipy.cluster.vq
 import matplotlib.pyplot as plot
 import numpy as np
+import sys
+import time
 
-with open("../data-x.txt") as fx, open("../data-y.txt") as fy:
+with open("../data-x-%s.txt" % sys.argv[1]) as fx, open("../data-y-%s.txt" % sys.argv[1]) as fy:
     data = []
     n, k = list(map(int, fx.readline().split()))
     n, k = list(map(int, fy.readline().split()))
     for i in range(n):
         data.append([float(fx.readline()), float(fy.readline())])
 data = np.array(data)
+
+start = time.time()
 kmeans = sklearn.cluster.KMeans(k, max_iter=300)
 labels = kmeans.fit_predict(data)
-means = kmeans.cluster_centers_
+print('%.4f' % ((time.time() - start) * 1000))
+# means = kmeans.cluster_centers_
 
-plot.scatter(data[:, 0], data[:, 1], c=labels)
-plot.scatter(means[:, 0], means[:, 1], linewidths=2)
-plot.show()
+# plot.scatter(data[:, 0], data[:, 1], c=labels)
+# plot.scatter(means[:, 0], means[:, 1], linewidths=2)
+# plot.show()
--- result/gen_data.py
+++ result/gen_data.py
@@ -3,6 +3,7 @@ import sklearn.cluster
 import scipy.cluster.vq
 import matplotlib.pyplot as plot
 import sys
+from math import sqrt
 
 
 n = int(sys.argv[1])
@@ -11,13 +12,13 @@ k = int(sys.argv[2])
 
 # Generate fake data
 data, labels = sklearn.datasets.make_blobs(
-    n_samples=n, n_features=m, centers=k)
+    n_samples=n, n_features=m, centers=k, center_box=(-20,20))
 
 kmeans = sklearn.cluster.KMeans(k, max_iter=300)
 kmeans.fit(data)
 means = kmeans.cluster_centers_
 
-with open("../data-x.txt", 'w') as fx, open("../data-y.txt", 'w') as fy:
+with open("../data-x-%d.txt" % n, 'w') as fx, open("../data-y-%d.txt" % n, 'w') as fy:
     print(n, k, file=fx)
     print(n, k, file=fy)
     for x, y in data:
